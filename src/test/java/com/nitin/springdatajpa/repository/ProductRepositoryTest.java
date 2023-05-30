a57749a2ac3e8a5e5331ca01ece3e0bdf24deca0
Nitin <nitin_gakhar@epam.com>
2023-05-30 16:49:08 -0400

    Adding Spring Data JPA repository testcases for findById and findAll

--- src/test/java/com/nitin/springdatajpa/repository/ProductRepositoryTest.java
+++ src/test/java/com/nitin/springdatajpa/repository/ProductRepositoryTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
 
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Annotate the specific test with @Rollback(false) inorder to persist the changes in DB
- * import org.springframework.test.annotation.Rollback;
+ * Uncomment import org.springframework.test.annotation.Rollback;
  */
 
 @DataJpaTest
@@ -25,15 +28,16 @@ class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
+        //@Rollback(false)
     void givenProduct_whenSave_thenReturnSavedProduct() {
         //given a product
         final Product product = Product.builder()
-                .name("Test Smart TV")
-                .sku("108TSMTV")
+                .name("Apple® AirPods (3rd generation)")
+                .sku("108APPAIRPOD")
                 .active(true)
-                .description("Test Smart TV description")
-                .price(new BigDecimal(100))
-                .imageURL("test_smart_tv.png")
+                .description("Apple® AirPods (3rd generation) with Lightning Charging Case")
+                .price(new BigDecimal(229))
+                .imageURL("test_smart_tv2.png")
                 .build();
         // when saving it
         final Product savedProduct = productRepository.save(product);
@@ -57,6 +61,27 @@ class ProductRepositoryTest {
             final Product modifiedProduct = productRepository.save(fetchedProduct);
             assertThat(modifiedProduct.getName()).isEqualTo(fetchedProduct.getName());
         }
+    }
+
+    @Test
+    void givenProduct_whenFindByID_thenFetchProduct() {
+        final Long productId = 1L;
+        //find product by ID
+        Optional<Product> product = productRepository.findById(productId);
+        if (product.isPresent()) {
+            final Product fetchedProduct = product.get();
+            assertThat(fetchedProduct.getId()).isNotNull();
+        }
+    }
+
+    @Test
+    void giveAllProducts_whenFindAll_thenSortResultsBySku() {
+        //find product by sorting based on SKU
+        List<Product> listOfProductsSortedBySku = productRepository.findAll(Sort.by(Sort.Direction.DESC, "description"));
+
+        //Usage of Sorting
+        listOfProductsSortedBySku.forEach(product -> System.out.println(product.getDescription()));
+
     }
 
 }
